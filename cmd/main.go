27c86760eb6c810b7d19ee864835752bea2cb588
ahappypie <bbagdasarian.scs@gmail.com>
2019-05-22 12:50:10 -0700

    Added logging with uber-go/zap. Changed epoch to 2019-01-01. Added sequence overflow error, subject to revision. Simplified makeTimestamp().

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"log"
+	"math"
 	"net"
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
@@ -16,7 +21,7 @@ import (
 const (
 	port = ":50001"
 
-	epoch = uint64(1543622400000)
+	epoch = uint64(1546300800000)
 
 	deploymentIdBits = uint64(10)
 	maxDeploymentId  = int64(-1) ^ (int64(-1) << deploymentIdBits)
@@ -35,36 +40,72 @@ var lastTimestamp = uint64(0)
 type Server struct{}
 
 func (s *Server) GetId(ctx context.Context, in *unica.UnicaRequest) (*unica.UnicaResponse, error) {
-	//log.Printf("Recevied request")
 	id, err := generate()
 	return &unica.UnicaResponse{Id: id}, err
 }
 
 func main() {
+	if os.Getenv("dev") == "true" {
+		logger, lerr := zap.NewDevelopment()
+		if lerr != nil {
+			log.Fatalf("can't initialize zap logger: %v", lerr)
+		}
+		defer logger.Sync()
+		zap.ReplaceGlobals(logger)
+	} else {
+		cfg := zap.Config{
+			Encoding:         "json",
+			Level:            zap.NewAtomicLevelAt(zapcore.DebugLevel),
+			OutputPaths:      []string{"stdout"},
+			ErrorOutputPaths: []string{"stderr"},
+			EncoderConfig: zapcore.EncoderConfig{
+				MessageKey: "message",
+
+				LevelKey:    "level",
+				EncodeLevel: zapcore.CapitalLevelEncoder,
+
+				TimeKey:    "time",
+				EncodeTime: zapcore.ISO8601TimeEncoder,
+
+				CallerKey:    "caller",
+				EncodeCaller: zapcore.ShortCallerEncoder,
+			},
+		}
+		logger, lerr := cfg.Build()
+		if lerr != nil {
+			log.Fatalf("can't initialize zap logger: %v", lerr)
+		}
+		defer logger.Sync()
+		zap.ReplaceGlobals(logger)
+	}
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("falied to bind: %v", err)
+		zap.L().Fatal("falied to bind", zap.Error(err))
 	}
 	s := grpc.NewServer()
 	unica.RegisterIdServiceServer(s, &Server{})
 	reflection.Register(s)
 	err2 := s.Serve(lis)
 	if err2 != nil {
-		log.Fatalf("failed to serve: %v", err2)
+		zap.L().Fatal("failed to serve", zap.Error(err2))
 	}
+	zap.L().Info("serving on", zap.String("port", port))
 }
 
 func generate() (uint64, error) {
 	var timestamp = makeTimestamp()
 
 	if timestamp < lastTimestamp {
-		log.Printf("clock is moving backwards. Waiting for %d milliseconds", (lastTimestamp - timestamp))
+		zap.L().Warn("clock is moving backwards", zap.Uint64("wait", lastTimestamp-timestamp))
 		timestamp = holdUntil(lastTimestamp)
 	}
 
 	if lastTimestamp == timestamp {
 		sequence = (sequence + 1) & sequenceMask
-		if sequence == 0 {
+		if sequence > (1 << sequenceBits) {
+			zap.L().Warn("sequence overloaded", zap.Uint64("sequence", sequence))
+			return 0, fmt.Errorf("sequence overloaded at %v, try again later", sequence)
+		} else if sequence == 0 {
 			timestamp = holdUntil(lastTimestamp)
 		}
 	} else {
@@ -76,6 +117,15 @@ func generate() (uint64, error) {
 	x := ((timestamp - epoch) << timestampShift) |
 		(did << deploymentIdShift) |
 		sequence
+
+	zap.L().Debug("components", zap.Uint64("epoch", epoch),
+		zap.Uint64("timestamp", timestamp),
+		zap.Uint64("timestampShift", timestampShift),
+		zap.Uint64("deploymentId", did),
+		zap.Uint64("deploymentIdShift", deploymentIdShift),
+		zap.Uint64("sequence", sequence),
+		zap.Uint64("id", x),
+		zap.Uint8("idBits", uint8(math.Round(math.Log2(float64(x))))))
 
 	return x, nil
 }
@@ -89,5 +139,5 @@ func holdUntil(ts uint64) uint64 {
 }
 
 func makeTimestamp() uint64 {
-	return uint64(time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)))
+	return uint64(time.Now().UnixNano() / 1e6)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ require (
 	github.com/golang/protobuf v1.2.0
 	github.com/google/pprof v0.0.0-20190109223431-e84dfd68c163 // indirect
 	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
 	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
 	golang.org/x/crypto v0.0.0-20190123085648-057139ce5d2b // indirect
 	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
